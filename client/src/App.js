2cac1e2d07bf7ce7b68d57391561b4a4f3d64fe6
Yuki Sato <sato.youxi@gmail.com>
2018-10-21 22:24:19 +0200

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './css/App.css';
 import './css/weather-icons.css';
+import Header from './Header';
 
 class App extends Component {
   state = {
@@ -48,22 +49,6 @@ class App extends Component {
       return <div className='spinner'>loading.....</div>
     }
 
-    let weatherIcon;
-    if (!this.state.currentDataLoad) {
-      let currentWeather = this.state.current.weather[0].main;
-      if (currentWeather === "Clear") {
-        weatherIcon = <i className="wi wi-day-sunny"></i>
-      } else if (currentWeather === "Clouds") {
-        weatherIcon = <i className="wi wi-cloudy"></i>
-      } else if (currentWeather === "Rain") {
-        weatherIcon = <i className="wi wi-rain"></i>
-      }
-    }
-
-    let currentTemp;
-    if (!this.state.currentDataLoad) {
-      currentTemp = Math.round(this.toCelcius(this.state.current.main.temp));
-    }
 
     let headerBackground = {
       background: '#0d395d'
@@ -72,11 +57,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header" style={headerBackground}>
-          <h1>{this.state.current.name}</h1>
-          <div>
-            <p>{currentTemp}&#176;</p>
-            <p>{weatherIcon}</p>
-          </div>
+          <Header
+            data={this.state.current}
+          />
         </header>
         <main>
 
